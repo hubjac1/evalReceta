5b6d565f8880a0427761a4471780e8db96c58985
Hubert Jacquin <Hubert.Jacquin@awtce.be>
2016-08-29 11:39:39 +0200

    Set up project Ruby/angular

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Receta::Application.config.secret_key_base = '403f9ecb11922001addd01159613193f2acd9f16e591363b0a743e7371ac1c2fbff0182e3817aed267ff3b3b84b9eb514bd57cee764574600bacb00afa727311'
