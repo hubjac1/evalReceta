da81e77937dce68bb27564f0038b917d6ec71617
Hubert Jacquin <Hubert.Jacquin@awtce.be>
2016-09-01 12:35:34 +0200

    view recipe details

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Receta::Application.routes.draw do
   root 'home#index'
-  resources :recipes, only: [:index]
+  resources :recipes, only: [:index, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
